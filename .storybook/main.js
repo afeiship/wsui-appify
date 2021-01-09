7f12993ae21b3f1806aa9c5a8077f2164f41a589
afeiship <1290657123@qq.com>
2021-01-09 19:46:24 +0800

    feat: update global scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   "stories": [
     "../__stories__/**/*.stories.mdx",
@@ -5,6 +7,23 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+    "@storybook/preset-scss"
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      // this is for both less and scss
+      test: /.*\.(?:le|c|sc)ss$/,
+      loaders: [
+        'style-loader',
+        'css-loader',
+        'scss-loader', // if scss then add 'sass-loader'
+      ]
+    });
+    config.plugins.push(new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash].css',
+      chunkFilename: '[id]-[contenthash].css',
+    }));
+    return config;
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '!style-loader!css-loader!sass-loader!../src/index.scss';
+
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
