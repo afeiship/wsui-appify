dbfee26acd126ce14b0595e746c84d2745212ba4
afeiship <1290657123@qq.com>
2021-01-10 12:00:17 +0800

    feat: toolbar

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import '!style-loader!css-loader!sass-loader!../src/index.scss';
+import '!style-loader!css-loader!sass-loader!../node_modules/@jswork/wsui-reset/dist/index.scss';
 import '!style-loader!css-loader!sass-loader!../__stories__/app.scss';
 
 
