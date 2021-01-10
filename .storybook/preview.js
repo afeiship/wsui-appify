71c42b6da38ee0e91aaba4126e07060809e6c41e
afeiship <1290657123@qq.com>
2021-01-10 13:58:48 +0800

    refactor: jsx to html compoentns

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
-import '!style-loader!css-loader!sass-loader!../src/index.scss';
 import '!style-loader!css-loader!sass-loader!../node_modules/@jswork/wsui-reset/dist/index.scss';
+import '!style-loader!css-loader!sass-loader!../node_modules/@jswork/wsui-shorty/dist/index.scss';
+import '!style-loader!css-loader!sass-loader!../src/index.scss';
 import '!style-loader!css-loader!sass-loader!../__stories__/app.scss';
 
 
